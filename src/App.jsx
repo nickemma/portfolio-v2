b5bd7f264675667760f7b8bdfb8427a394240c77
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2024-07-16 11:59:36 +0100

    Added suspense and lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import { useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Home from './components/home/Home';
-import Header from './components/header/Header';
-import Contact from './components/contact/Contact';
-import About from './components/about/About';
-import Portfolio from './components/portfolio/Portfolio';
+import { Suspense, lazy, useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/home/Home";
+import Header from "./components/header/Header";
+import Contact from "./components/contact/Contact";
+// import About from './components/about/About';
+// import Portfolio from './components/portfolio/Portfolio';
+const About = lazy(() => import("./components/about/About"));
+const Portfolio = lazy(() => import("./components/portfolio/Portfolio"));
 
 const App = () => {
   const [menuState, setMenuState] = useState(false);
@@ -22,14 +24,16 @@ const App = () => {
         handleHamburger={handleMenuState}
         handleLinkClick={handleLinkClick}
       />
-      <Routes>
-        <Route path="/" element={<Home menu={menuState} />} />
-        <Route path="/About" element={<About menu={menuState} />} />
-        <Route path="/Portfolio" element={<Portfolio menu={menuState} />} />
-        <Route path="/Contact" element={<Contact menu={menuState} />} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Home menu={menuState} />} />
+          <Route path="/About" element={<About menu={menuState} />} />
+          <Route path="/Portfolio" element={<Portfolio menu={menuState} />} />
+          <Route path="/Contact" element={<Contact menu={menuState} />} />
+        </Routes>
+      </Suspense>
     </>
   );
-}
+};
 
 export default App;
